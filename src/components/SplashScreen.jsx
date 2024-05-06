bc17cec08fd23737e826bc2e782ea69217b7c438
Akkun <akkunlab@gmail.com>
2024-05-06 22:23:51 +0900

    feat: add SplashScreen component

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+
+class SplashScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: true };
+  }
+  
+  /** 2秒後に非表示 */
+  componentDidMount() {
+    this.timer = setTimeout(() => {
+      this.setState({ visible: false });
+      console.log('aa');
+    }, 2000);
+  }
+  
+  /** タイマーをクリア */
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  /** 画面に表示 */
+  render() {
+    if (!this.state.visible) {
+      return null;
+    }
+
+    return (
+      <div style={{ 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: 'lightblue'
+      }}>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+}
+
+export default SplashScreen;
