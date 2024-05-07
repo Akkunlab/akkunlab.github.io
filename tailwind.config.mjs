44b80d56522cedd00456670265ac8a5b4116b5be
Akkun <akkunlab@gmail.com>
2024-05-07 20:50:58 +0900

    style: update SplashScreen class

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,16 @@ import daisyui from 'daisyui'
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+  daisyui: {
+    themes: [
+      {
+        theme: {
+          'primary': '#0015ca',
+          'primary-content': '#000',
+        },
+      },
+    ],
+  },
   plugins: [
     daisyui,
   ],
