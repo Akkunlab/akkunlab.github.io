a1dbe6fc403f90164456855f12c8defe42b078cf
Akkun <akkunlab@gmail.com>
2024-08-12 21:28:03 +0900

    feat: update npm dependencies and add astro-robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
 import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), playformCompress(), icon()]
+  site: 'https://akkunlab.pages.dev/',
+  integrations: [
+    tailwind(),
+    playformCompress(),
+    icon(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          disallow: '/',
+        },
+      ],
+    }),
+  ]
 });
