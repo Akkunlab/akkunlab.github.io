ab4e9a45ba8c7a0b33e15514970d3ae0b7cd8c1f
Akkun <akkunlab@gmail.com>
2025-03-31 16:58:28 +0900

    feat: update site data with author details and add social media links section

--- src/constants/siteData.ts
+++ src/constants/siteData.ts
@@ -1,6 +1,16 @@
 export const SITE_TITLE: string = '岸本篤 / Atsushi Kishimoto Portfolio';
-export const SITE_DESCRIPTION: string = '人々の幸せを実現するためにテクノロジーを駆使するエンジニア, 岸本篤のポートフォリオサイト.';
+export const SITE_DESCRIPTION: string = 'テクノロジーとアートの融合で、人々に感動と幸せを届けるクリエイター、岸本篤のポートフォリオサイト。';
 export const SITE_URL: string = 'https://akkunlab.pages.dev/';
-export const AUTHOR: string = '岸本篤';
+
+export const AUTHOR: string = '岸本 篤';
+export const AUTHOR_ENGLISH: string = 'Atsushi Kishimoto';
+export const AUTHOR_SUMMARY: string = '江蘇省生まれ。小学生の頃からモノづくりに取り組む。福島工業高等専門学校を卒業し、茨城大学工学部を経て、筑波大学大学院情報学学位プログラムに所属。パソコン甲子園、技育展、高専プロコンに出場し、2021年にAWS Robot Delivery Challenge準優勝を果たす。2023年には常陸frogs（現・茨城frogs）5期生に選抜される。';
+
 export const X_ID: string = '@akkun_lab';
 export const PRIMARY_COLOR: string = '#0015ca';
+
+export const HERO_TITLE_LINES: Array<string> = [
+  'テクノロジーと',
+  'アートの融合で',
+  '人々を幸せに'
+];
