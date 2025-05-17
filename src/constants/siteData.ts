08992bdfaa3df5ee180d948222c732aa1c390470
Akkun <38970950+Akkunlab@users.noreply.github.com>
2025-05-17 10:27:20 +0900

    fix: update social media descriptions and colors for consistency; change hero image format

--- src/constants/siteData.ts
+++ src/constants/siteData.ts
@@ -7,7 +7,7 @@ export const AUTHOR_ENGLISH: string = 'Atsushi Kishimoto';
 export const AUTHOR_SUMMARY: string = '江蘇省生まれ。小学生の頃からモノづくりに取り組む。福島工業高等専門学校を卒業し、茨城大学工学部を経て、筑波大学大学院情報学学位プログラムに所属。パソコン甲子園、技育展、高専プロコンに出場し、2021年にAWS Robot Delivery Challenge準優勝を果たす。2023年には常陸frogs（現・茨城frogs）5期生に選抜される。';
 
 export const X_ID: string = '@akkun_lab';
-export const PRIMARY_COLOR: string = '#0015ca';
+export const PRIMARY_COLOR: string = '#0015CA';
 
 export const HERO_TITLE_LINES: Array<string> = [
   'テクノロジーと',
