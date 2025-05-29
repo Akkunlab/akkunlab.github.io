c54e2dffcb662bab7e61bf90aeadc5b10cae3bb6
Akkun <akkunlab@gmail.com>
2025-05-29 21:41:21 +0900

    feat: integrate SNS_LINKS constant for social media links in Footer and index components

--- src/constants/siteData.ts
+++ src/constants/siteData.ts
@@ -21,23 +21,60 @@ export const NAV_ITEMS = [
     href: '/#works',
     icon: 'work-outline',
     activeIcon: 'work'
-  },
-  {
+  }, {
     label: '活動',
     href: '/#activities',
     icon: 'event-available-outline-rounded',
     activeIcon: 'event-available-rounded'
-  },
-  {
+  }, {
     label: 'プロフィール',
     href: '/#profile',
     icon: 'account-circle-outline',
     activeIcon: 'account-circle'
-  },
-  {
+  }, {
     label: 'リンク',
     href: '/#links',
     icon: 'link-rounded',
     activeIcon: 'link-rounded'
   }
+];
+
+export const SNS_LINKS = [
+  {
+    name: 'X',
+    description: '日常の気づきをつぶやいています！',
+    url: 'https://x.com/akkun_lab',
+    icon: 'twitter',
+    color: '#000000',
+  }, {
+    name: 'Instagram',
+    description: 'アートや日々の活動をシェアしています！',
+    url: 'https://www.instagram.com/akkunlab',
+    icon: 'instagram',
+    color: '#E1306C',
+  }, {
+    name: 'Facebook',
+    description: '活動報告をお届けしています！',
+    url: 'https://www.facebook.com/akkunlab',
+    icon: 'facebook',
+    color: '#1877F2',
+  }, {
+    name: 'GitHub',
+    description: '作品のソースコードを公開しています！',
+    url: 'https://github.com/Akkunlab',
+    icon: 'github',
+    color: '#24292E',
+  }, {
+    name: 'YouTube',
+    description: '作品の紹介動画を配信しています！',
+    url: 'https://www.youtube.com/@akkunlab',
+    icon: 'youtube',
+    color: '#FF0000',
+  }, {
+    name: 'Email',
+    description: 'お問い合わせはこちらから！',
+    url: 'mailto:akkunlab@gmail.com',
+    icon: 'envelope',
+    color: '#04802A',
+  },
 ];
