f072dfb6d315a95ae8cf0554a6ab1402cb4fa760
Atsushi Kishimoto <akkunlab@gmail.com>
2025-12-27 01:06:04 +0900

    fix: update SITE_URL to correct domain

--- src/constants/siteData.ts
+++ src/constants/siteData.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE: string = '岸本篤 / Atsushi Kishimoto Portfolio';
 export const SITE_DESCRIPTION: string = 'テクノロジーとデザインの融合で、人々に感動と幸せを届けるクリエイター、岸本篤のポートフォリオサイト。';
-export const SITE_URL: string = 'https://akkunlab.pages.dev/';
+export const SITE_URL: string = 'https://akkunlab.dev/';
 
 export const AUTHOR: string = '岸本 篤';
 export const AUTHOR_ENGLISH: string = 'Atsushi Kishimoto';
